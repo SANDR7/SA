008ba892fd31c6bb468ebeb991436315287185a6
Sander van Ast <sandervanast1@gmail.com>
2022-06-30 23:10:07 +0200

    feat: :bricks: New schemas
    Related schema in studio

---
+++ sanity/schemas/blogs.js
@@ -0,0 +1,14 @@
+export default {
+  name: "blogs",
+  title: "Blogs",
+  type: "document",
+  fields: [
+    //   Default values
+    {
+      name: "title",
+      title: "Title",
+      description: "Subjects name",
+      type: "string",
+    },
+  ],
+};
---
+++ sanity/schemas/components/skills.js
@@ -0,0 +1,40 @@
+export default {
+  name: "skills",
+  title: "Skills",
+  type: "document",
+  fields: [
+    //   Default values
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "type",
+      title: "Type",
+      type: "array",
+      of: [{ type: "string" }],
+      options: {
+        list: [
+          { value: "program", title: "Software program" },
+          { value: "language", title: "Programming language" },
+          { value: "tool", title: "Development Tool" },
+        ],
+      },
+    },
+    {
+      name: "tags",
+      title: "Tags",
+      type: "array",
+	  of: [{type: 'string'}],
+      options: {
+        layout: "tags",
+      },
+    },
+    {
+      name: "logo",
+      title: "Logo",
+      type: "image",
+    },
+  ],
+};
