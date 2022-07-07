dfd2316a53b52f8b75f109af0e76009d2028b7ca
Sander <sandervanast1@gmail.com>
2022-07-07 12:02:21 +0200

    feat: :sparkles: fetch data from sanity cms
    using catchall api to fetch data endpoint

--- nextjs/src/server/router/sanity/projects.ts
+++ nextjs/src/server/router/sanity/projects.ts
@@ -2,20 +2,28 @@ import groq from "groq";
 import { sanityClient } from "../../../utils/sanity";
 import { createRouter } from "../context";
 
-interface projectProps {
-	title: string,
-	excerpt?: string
+interface Project {
+  title: string;
+  excerpt?: string;
+  _id: string;
+  image?: string;
 }
-
 
 export const SanityRouter = createRouter().query("projects", {
   async resolve() {
-    const projects: projectProps[] = await sanityClient.fetch(
-      groq`*[_type == "projects"] {excerpt, title }`
+    const projects: Project[] = await sanityClient.fetch(
+      groq`*[_type == "projects"] | order(finished_date asc) {
+        _id,
+        excerpt,
+        'type': type[0],
+        title,
+        'thumbnail': {
+          'image': thumbnail.asset->url,
+          'caption': thumbnail.caption
+        }
+      }`
     );
 
-    return {
-      projects,
-    };
+    return { projects };
   },
 });
