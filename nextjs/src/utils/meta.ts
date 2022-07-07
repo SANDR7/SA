5a1d8020ddef4c2905aebec994c7110515b0e219
Sander van Ast <sandervanast1@gmail.com>
2022-07-07 17:57:32 +0200

    chore: :speech_balloon: Adding meta data to website
    object with meta data inc. images

---
+++ nextjs/src/utils/meta.ts
@@ -0,0 +1,28 @@
+export const meta = {
+  title: "Sander van Ast — Web Designer, Developer, Consumer",
+  description:
+    "I’m Sander a enthousiast designer and web developer with experience in making things look good.",
+  keywords: [
+    "Portfolio",
+    "Resume",
+    "Sander van Ast",
+    "Web designer",
+    "Web developer",
+  ],
+  domain: "https://sandervanast.website/",
+  favicon: {
+    md: "/SA.png", // 512
+    dark: "SA dark.png", // 512
+  },
+  colors: {
+    theme: "#ff7b47",
+    tile: "#f8f9f9",
+    tab: "#2e2e2e",
+  },
+  thumbnail: {
+    width: "1820",
+    height: "904",
+    banner: "/banner.png",
+    twitter: "etc.",
+  },
+};
