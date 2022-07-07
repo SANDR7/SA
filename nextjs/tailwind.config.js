873462e0ca1b0db48008bae587512a72f2e7b3c9
Sander <sandervanast1@gmail.com>
2022-07-07 11:59:59 +0200

    style: :art: initial tailwind setup

---
+++ nextjs/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media',
+  mode: 'jit',
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
