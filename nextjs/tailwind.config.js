17cecfc6278eddc6f81ff42db31d7a766dd4620d
Sander van Ast <sandervanast1@gmail.com>
2022-07-08 12:01:43 +0200

    style: :lipstick: responsive style guide
    tailwind style config

--- nextjs/tailwind.config.js
+++ nextjs/tailwind.config.js
@@ -26,6 +26,18 @@ module.exports = {
         800: "#979797",
       },
     },
+    screens: {
+      mobile: "320px",
+      // => @media (min-width: 640px) { ... }
+      tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
     fontFamily: {
       sans: ["rubik", "sans-serif"],
     },
