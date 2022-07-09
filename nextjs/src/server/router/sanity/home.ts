b11c123fb19a747108a89c1b5db65af6d61e6629
Sander van Ast <sandervanast1@gmail.com>
2022-07-09 22:58:15 +0200

    feat: :building_construction: adding demo link to backend
    adding link to consume in the frontend

--- nextjs/src/server/router/sanity/home.ts
+++ nextjs/src/server/router/sanity/home.ts
@@ -13,11 +13,12 @@ export const SanityRouter = createRouter()
   .query("projects", {
     async resolve() {
       const projects: Project[] = await sanityClient.fetch(
-        groq`*[_type == "projects"][0..1] | order(finished_date asc) {
+        groq`*[_type == "projects"][0..1] | order(finished_date desc) {
         _id,
         excerpt,
         'type': type[0],
         title,
+       'production_link': [client_link, saved_link, live_link],
         'thumbnail': {
           'image': thumbnail.asset->url,
           'caption': thumbnail.caption
