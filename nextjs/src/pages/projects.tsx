5d8f238c977f1e9d543fe3265e60fc427a7176ed
Sander van Ast <sandervanast1@gmail.com>
2022-07-13 13:58:06 +0200

    feat: :sparkles: basic project page

--- nextjs/src/pages/projects.tsx
+++ nextjs/src/pages/projects.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
-import PageContainer from '../components/layout'
-import { meta } from '../data/meta'
+import { createSSGHelpers } from "@trpc/react/ssg";
+import type { GetStaticPropsContext } from "next";
+import dynamic from "next/dynamic";
+import { UseQueryResult } from "react-query";
+import superjson from "superjson";
+
+import PageContainer from "../components/layout";
+import { meta } from "../data/meta";
+import { appRouter } from "../server/router";
+import { Sanity } from "../types/sanity/home.queries";
+import { trpc } from "../utils/trpc";
+
+const ProjectCard = dynamic(
+  () => import("../components/ui/section/project/card")
+);
+const SectionHeader = dynamic(() => import("../components/ui/section/header"));
 
 const Projects = () => {
+  const { data: projects, isLoading }: UseQueryResult<Sanity.Projects.Home[]> =
+    trpc.useQuery(["projects.all"]);
   return (
-	<PageContainer title={meta.title + 'Projects'}>Projects</PageContainer>
-  )
+    <PageContainer title={meta.title + "Projects"}>
+      <section>
+        <SectionHeader title="Projects collection" />
+        {!!projects &&
+          projects.slice(0,6).map((project) => (
+            <section
+              about={project.title}
+              key={project._id}
+              className="my-[60px] group relative laptop:flex"
+            >
+              <ProjectCard {...project} />
+            </section>
+          ))}
+      </section>
+    </PageContainer>
+  );
+};
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const ssg = createSSGHelpers({
+    router: appRouter,
+    ctx: {} as any,
+    transformer: superjson, // optional - adds superjson serialization
+  });
+  // prefetch from server
+  await ssg.fetchQuery("projects.all");
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+    },
+    revalidate: 30,
+  };
 }
 
-export default Projects
+export default Projects;
--- nextjs/src/server/router/sanity/projects.ts
+++ nextjs/src/server/router/sanity/projects.ts
@@ -6,7 +6,7 @@ import { createRouter } from "../context";
 export const ProjectRouter = createRouter().query("home", {
   async resolve() {
     const projects = await sanityClient.fetch<Sanity.Projects.Home>(
-      groq`*[_type == "projects"] | order(finished_date desc)[0..1] {
+      groq`*[_type == "projects" && highlighted] | order(finished_date desc)[0..1] {
         _id,
         'slug': slug.current,
         excerpt,
@@ -22,4 +22,23 @@ export const ProjectRouter = createRouter().query("home", {
 
     return projects;
   },
+}).query('all', {
+  async resolve() {
+    const projects = await sanityClient.fetch<Sanity.Projects.Home>(
+      groq`*[_type == "projects"] | order(finished_date desc)[0..6] {
+        _id,
+        'slug': slug.current,
+        excerpt,
+        'type': type[0],
+        title,
+       'production_link': [client_link, saved_link, live_link],
+        'thumbnail': {
+          'image': thumbnail.asset->url,
+          'caption': thumbnail.caption
+        }
+      } `
+    );
+
+    return projects;
+  }
 });
